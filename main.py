8c635235ae027ae45590ae428012fd24d98823f1
Courey J <coureyfrance@gmail.com>
2026-09-10 14:58:26 -0400

    Finally debugging my notebook

---
+++ main.py
@@ -0,0 +1,20 @@
+# GLAB 385.2.1 - Lists & Methods
+
+#python3 -m venv <virtualEnvName .venv> 
+'''
+CREATING VIRTUAL ENVIRONMENT
+1. Open your terminal.
+2. Navigate to your project directory using the cd command.
+3. Run the following command to create a virtual environment:
+   python3 -m venv .venv
+4. Activate the virtual environment: makes all modules installed in the .venv folder 
+    available for use in your project.
+   - USE this command to activate
+     source .venv/bin/activate
+- to deactivate the virtual environment, simply run:
+   deactivate
+
+INSTALLING PACKAGES IN ACTIVE ENV
+1. Make sure your virtual environment is activated.
+2. Use command pip install (insert name of package)
+'''
